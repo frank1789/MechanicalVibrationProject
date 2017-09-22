f33d890cf4cab36897fc6e9726ee3cfe1d931cbb
frank1789 <francesco.argentieri89@gmail.com>
2017-09-22 10:11:50 +0200

    add documentations

--- computeforce.m
+++ computeforce.m
@@ -1,26 +1,26 @@
 function [ F ] = computeforce( pDatatset )
 %Computeforce the function compute the force applied at system passed
-%argument voltage from dataset and return a matrix with force component
-                                                 
+% argument voltage from dataset and return a matrix with force component
 % costant from readme
-% k_a = 2;                               % servo amp gain
-% k_t = 0.1;                             % Servo motor Torque
-% k_mp = 26.25;                          % Motor Pinioon pitch radius inverse 
-
-% F = zeros(length(data_steps.time.t), 3);         % initialize F's vector
-
+% k_a = 2;                     % servo amp gain [A/V]
+% k_t = 0.1;                   % Servo motor Torque [Nm/A]
+% k_mp = 26.25;                % Motor Pinioon pitch radius inverse [1/m]
+%
+% initialize F's vector
+% F = zeros(length(data_steps.time.t), 3);
+%
 % temporary vector of force
-% f = (Inputdata.k_a * Inputdata.k_t * Inputdata.k_mp) * data_steps.voltage.v;   
-
+% f = (Inputdata.k_a * Inputdata.k_t * Inputdata.k_mp) * data_steps.voltage.v;
+%
 % return matrix of force
-% F(:,1) = f;                                      
+% F(:,1) = f;
 
 % initialize F's vector
 F = zeros(length( pDatatset.time.t), 3);
 
-k_a = 2;                               % servo amp gain
-k_t = 0.1;                             % Servo motor Torque
-k_mp = 26.25;                          % Motor Pinioon pitch radius inverse 
+k_a = 2;                     % servo amp gain [A/V]
+k_t = 0.1;                   % Servo motor Torque [Nm/A]
+k_mp = 26.25;                % Motor Pinioon pitch radius inverse [1/m]
 
 % tmp vector 
 f = (k_a * k_t * k_mp) * pDatatset.voltage.v;
