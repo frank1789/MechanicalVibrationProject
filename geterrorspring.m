9b3d6507e304d8c94b32c716db1fd2d8f4c07ef6
frank1789 <francesco.argentieri89@gmail.com>
2017-09-23 18:23:10 +0200

    :sparkles: solved error with nominal voltage

--- geterrorspring.m
+++ geterrorspring.m
@@ -9,6 +9,6 @@ R31_per = abs((pRatio_k3_k1 - nominalRatio_31) * 100 / (pRatio_k3_k1));
 R32_per = abs((pRatio_k3_k2 - nominalRatio_32) * 100 / (pRatio_k3_k2));
 display(R31_per);display( R32_per);
 
-g_v_per = abs(100* (pGain_v - pDataset.voltage.v(1)) / (pDataset.voltage.v(1)));
+g_v_per = abs(100* (5.25 - pGain_v) / (5.25));
 display(g_v_per);
 end
