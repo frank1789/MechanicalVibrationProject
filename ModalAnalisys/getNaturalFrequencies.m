53a36588fb11769421d1c5be4b350df7bd62f86a
frank1789 <francesco.argentieri89@gmail.com>
2017-09-25 10:28:16 +0200

    :bug: find right frquencies, but not modes

--- ModalAnalisys/getNaturalFrequencies.m
+++ ModalAnalisys/getNaturalFrequencies.m
@@ -1,16 +1,31 @@
 function [M,K,freqs,modes] = getNaturalFrequencies( pOptimValue, pInputdata)
 
-m1 = pOptimValue(1);
-m2 = pOptimValue(2);
-m3 = pOptimValue(3);
+if length(pOptimValue) > 6
+    fprintf('Compute modes and natural frequencies for full optiaml value:\n');
 
-c1 = pOptimValue(4);
-c2 = pOptimValue(5);
-c3 = pOptimValue(6);
+    m1 = pOptimValue(1);
+    m2 = pOptimValue(2);
+    m3 = pOptimValue(3);
 
-k1 = pInputdata.stiffness.k1;
-k2 = pInputdata.stiffness.k2;
-k3 = pInputdata.stiffness.k3;
+    c1 = pOptimValue(4);
+    c2 = pOptimValue(5);
+    c3 = pOptimValue(6);
+
+    k1 = pInputdata.stiffness.k1;
+    k2 = pInputdata.stiffness.k2;
+    k3 = pInputdata.stiffness.k3;
+else
+    fprintf('Compute modes and natural frequencies for proportional optiaml value:\n');
+    
+    m1 = pOptimValue(1);
+    m2 = pOptimValue(2);
+    m3 = pOptimValue(3);
+
+    k1 = pInputdata.stiffness.k1;
+    k2 = pInputdata.stiffness.k2;
+    k3 = pInputdata.stiffness.k3;
+end
+ 
 
 % assemble mass matrix [M]
 M = [m1 0 0; 0 m2 0; 0 0 m3];
@@ -28,12 +43,11 @@ display(K);
 freqs = zeros(3,1);
 
 
-%M = [m1,0;0,m2];
-%K = [k1+k2,-k2;-k2,k2+k3];
 [V,D] = eig(K,M);
 for i = 1:length(M)
     freqs(i) = sqrt(D(i,i));
 end
+
 modes = V;
  
 end
