6d0a0ee66b78a0c6c701769b776ad181190f5913
frank1789 <francesco.argentieri89@gmail.com>
2017-09-25 11:15:54 +0200

    :sparkles: solved problem of eigenvectors

--- ModalAnalisys/getNaturalFrequencies.m
+++ ModalAnalisys/getNaturalFrequencies.m
@@ -1,53 +1,69 @@
-function [M,K,freqs,modes] = getNaturalFrequencies( pOptimValue, pInputdata)
+function [ freqs, modes ] = getNaturalFrequencies(pOptimValue, pInputdata)
+% getNaturalFrequencies compute vibration modes return as modes and compute
+% naturals frequencies return as freqs. 
+% Passed:
+%   as argument pOptimValue -> mass retrived from optimztion 
+%   as argument pInputdata -> stiffness of spring from readme 
+% instaziate local variable from Optimalvalue and data
+% m1 = pOptimValue(1);
+% m2 = pOptimValue(2);
+% m3 = pOptimValue(3);
+% 
+% k1 = pInputdata.stiffness.k1;
+% k2 = pInputdata.stiffness.k2;
+% k3 = pInputdata.stiffness.k3;
+% 
+% % assemble mass matrix [M]
+% M = [m1 0 0; 0 m2 0; 0 0 m3];
+%
+% assemble stifness matrix [K]
+% K = [k1 -k1 0; -k1 k2+k1 -k2; 0 -k2 k2+k3];
+%
+% instaziate return variable
+% freqs = zeros(3,1);
+%
+% produces a diagonal matrix D of generalized eigenvalues and a full matrix V
+% [ V, D ] = eig(K, M);
+%
+% compute the frequencies
+% for i = 1:length(M)
+%     freqs(i) = sqrt(D(i,i));
+%     % compute the modes
+%     V(1:3,i) = V(1:3,i)./V(1,i);
+% end
+% 
+% return modes
+% modes = V;
 
-if length(pOptimValue) > 6
-    fprintf('Compute modes and natural frequencies for full optiaml value:\n');
-
+% instaziate local variable from Optimalvalue and data
     m1 = pOptimValue(1);
     m2 = pOptimValue(2);
     m3 = pOptimValue(3);
 
-    c1 = pOptimValue(4);
-    c2 = pOptimValue(5);
-    c3 = pOptimValue(6);
-
     k1 = pInputdata.stiffness.k1;
     k2 = pInputdata.stiffness.k2;
     k3 = pInputdata.stiffness.k3;
-else
-    fprintf('Compute modes and natural frequencies for proportional optiaml value:\n');
-    
-    m1 = pOptimValue(1);
-    m2 = pOptimValue(2);
-    m3 = pOptimValue(3);
-
-    k1 = pInputdata.stiffness.k1;
-    k2 = pInputdata.stiffness.k2;
-    k3 = pInputdata.stiffness.k3;
-end
- 
 
 % assemble mass matrix [M]
 M = [m1 0 0; 0 m2 0; 0 0 m3];
-display(M);
-
-% assemble damping matric [C]
-C = [c1 0 0; 0 c2 0; 0 0 c3];
-display(C);
 
 % assemble stifness matrix [K]
 K = [k1 -k1 0; -k1 k2+k1 -k2; 0 -k2 k2+k3];
-display(K);
 
-
+% instaziate return variable
 freqs = zeros(3,1);
 
+% produces a diagonal matrix D of generalized eigenvalues and a full matrix V
+[ V, D ] = eig(K, M);
 
-[V,D] = eig(K,M);
+% compute the frequencies
 for i = 1:length(M)
     freqs(i) = sqrt(D(i,i));
+    % compute the modes eignvectors
+    V(1:3,i) = V(1:3,i)./V(1,i);
 end
 
+% return modes
 modes = V;
- 
+
 end
