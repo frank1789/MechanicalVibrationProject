c0dc71559797a6bcc73a032af0b18bee6a6a2ea3
frank1789 <francesco.argentieri89@gmail.com>
2017-09-29 18:10:25 +0200

    :sparkles: work fine

--- geterrorspring.m
+++ geterrorspring.m
@@ -1,14 +1,18 @@
-function [] = geterrorspring(pGain_v, pRatio_k3_k2, pRatio_k3_k1, pInputdata, pDataset)
+function [ g_v_per, R31_per, R32_per ] = geterrorspring(pGain_v, pRatio_k3_k2, pRatio_k3_k1, pInputdata)
+%geterrorspring compute the ratio between nominal value of spring and ratio
+% between nomial gain value and coputed gain value
 
-
+% compute nomial ratio 
 nominalRatio_31 = pInputdata.stiffness.k3 / pInputdata.stiffness.k1;
 nominalRatio_32 = pInputdata.stiffness.k3 / pInputdata.stiffness.k2;
 
-
+% compute error between nominal and estimated spring
 R31_per = abs((pRatio_k3_k1 - nominalRatio_31) * 100 / (pRatio_k3_k1));
 R32_per = abs((pRatio_k3_k2 - nominalRatio_32) * 100 / (pRatio_k3_k2));
-display(R31_per);display( R32_per);
+display(R31_per);
+display( R32_per);
 
-g_v_per = abs(100* (5.25 - pGain_v) / (5.25));
+% compute error between nominal and estimated gain
+g_v_per = abs(100* (pInputdata.voltage.v - pGain_v) / (pInputdata.voltage.v));
 display(g_v_per);
 end
