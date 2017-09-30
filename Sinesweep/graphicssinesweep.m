d0171ff9af11ece16630823bc330b0bb31f25b96
frank1789 <francesco.argentieri89@gmail.com>
2017-09-30 17:37:37 +0200

    sparkles: solved passed argument if-statement

--- Sinesweep/graphicssinesweep.m
+++ Sinesweep/graphicssinesweep.m
@@ -1,9 +1,23 @@
-function graphicssinesweep(pCount, pEstimatedtf, pNamefile)
+function graphicssinesweep(pCount, pNamefile, pEstimatedtf, pEstimatedtf2)
 
+if ~exist('pEstimatedtf2','var')|| isempty(pEstimatedtf2)
     figure();
     bodeplot(pEstimatedtf, logspace(0,2,1000));
     grid on;
     namefile = strcat(pNamefile, ['bodediagram' int2str(pCount)]);
 %     legend('free damping', 'proportinal damping','Location','west');
 %     saveas(gcf,namefile,'epsc')
+    
+    
+    
+else   
+    figure();
+    bodeplot(pEstimatedtf,pEstimatedtf2, logspace(0,2,1000));
+    grid on;
+    namefile = strcat(pNamefile, ['bodediagram' int2str(pCount)]);
+%     legend('free damping', 'proportinal damping','Location','west');
+%     saveas(gcf,namefile,'epsc')    
+    
+end
+
 end
