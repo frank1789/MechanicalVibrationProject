fd38f8fdf52fe651971b3778572b6ac9863ddb39
frank1789 <francesco.argentieri89@gmail.com>
2017-09-30 18:32:05 +0200

    add doc and storing file eps

--- Sinesweep/graphicssinesweep.m
+++ Sinesweep/graphicssinesweep.m
@@ -1,23 +1,24 @@
 function graphicssinesweep(pCount, pNamefile, pEstimatedtf, pEstimatedtf2)
+%graphicssinesweep generate plot of estimate transfer function compute by
+%tfest and store images.eps
 
 if ~exist('pEstimatedtf2','var')|| isempty(pEstimatedtf2)
+    % print single tf
     figure();
     bodeplot(pEstimatedtf, logspace(0,2,1000));
     grid on;
+    str = sprintf('Bode Diagram %s', pNamefile);
+    title(str);
     namefile = strcat(pNamefile, ['bodediagram' int2str(pCount)]);
-%     legend('free damping', 'proportinal damping','Location','west');
-%     saveas(gcf,namefile,'epsc')
-    
-    
-    
-else   
+    saveas(gcf,namefile,'epsc');
+else
+    %print overlap tf
     figure();
     bodeplot(pEstimatedtf,pEstimatedtf2, logspace(0,2,1000));
     grid on;
     namefile = strcat(pNamefile, ['bodediagram' int2str(pCount)]);
-%     legend('free damping', 'proportinal damping','Location','west');
-%     saveas(gcf,namefile,'epsc')    
-    
+    legend('sine sweep slow', 'sine sweep fast');
+    saveas(gcf,namefile,'epsc');
 end
 
 end
