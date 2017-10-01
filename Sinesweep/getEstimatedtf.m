bb34d729594ed7d834b47d31ecd84a285e7da9d5
frank1789 <francesco.argentieri89@gmail.com>
2017-10-01 10:36:27 +0200

    :bug: fittin model value correct, but not fix

--- Sinesweep/getEstimatedtf.m
+++ Sinesweep/getEstimatedtf.m
@@ -1,10 +1,12 @@
 function [ estimatedtf ] = getEstimatedtf(pCount, pTime, pDisplacement, pVolt, pSintype)
 
+% % sampling period
+Ts = pTime(450) - pTime(449);
 
 if pSintype == 'slow'
     
     % sampling period
-    Ts = pTime(2);
+    %     Ts = pTime(2);
     
     % creates an iddata object containing a time-domain output signal y and
     % input signal u, respectively. Ts specifies the sample time of the
@@ -16,11 +18,13 @@ if pSintype == 'slow'
     nz = 5 - pCount;
     estimatedtf = tfest(sinesweepslow, np, nz);
     display(estimatedtf);
-
+    
 elseif pSintype == 'fast'
     
     % sampling period
     Ts = pTime(2);
+    
+    
     
     % creates an iddata object containing a time-domain output signal y and
     % input signal u, respectively. Ts specifies the sample time of the
@@ -30,8 +34,19 @@ elseif pSintype == 'fast'
     % estimates a transfer function containing nz zeros
     np = 6;
     nz = 5 - pCount;
-    estimatedtf = tfest(sinesweepfast, np, nz);
-    display(estimatedtf);
+    if nz > 2
+        estimatedtf = tfest(sinesweepfast, np, nz);
+        display(estimatedtf);
+    else
+%         load x3ssf.mat
+%         Ts == Ts_ssf
+%         Ts == 0.0050
+%         Ts_ssf == 0.0050
+        Ts = pTime(450) - pTime(449);
+        sinesweepfast = iddata(pDisplacement, pVolt, Ts);
+        estimatedtf = tfest(sinesweepfast, np, nz);
+        display(estimatedtf);
+    end
 else
     spritn('sinesweep not specified')
 end
